bba8a7412e71f07337a239a1226aaa2a54652c54
Alec Von Arx <al.vonarx+fridge-dev@gmail.com>
2025-02-22 19:00:53 -0800

    Add cp commands to wasm build

--- wasm-build.sh
+++ wasm-build.sh
@@ -3,3 +3,7 @@
 set -euxo pipefail
 
 cargo build --target wasm32-unknown-unknown --release
+
+cp ./target/wasm32-unknown-unknown/release/caterpillar.wasm ./docs/
+cp ./target/wasm32-unknown-unknown/release/mastermind.wasm ./docs/
+cp ./target/wasm32-unknown-unknown/release/turn-time-tracker.wasm ./docs/
